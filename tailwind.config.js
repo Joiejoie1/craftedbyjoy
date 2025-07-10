ce1ad2113a25a4d869247a642f48867a2710c32f
Joiejoie1 <soniqjjj@gmail.com>
2025-07-10 14:57:10 +0100

    Set up Tailwind CSS with Vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
